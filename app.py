1f3b2d7dbc055a91ec0e9f93ad18bf354638a086
HScallop <f54076120@gs.ncku.edu.tw>
2022-01-02 22:59:54 +0800

    all stories done.

--- app.py
+++ app.py
@@ -10,25 +10,132 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from fsm import TocMachine
 from utils import send_text_message
 
+#from libretranslatepy import LibreTranslateAP
+
 load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=["user", "start", "language", "chinese", "german", "french", "prison",
+    "prison", "nobody", "friend", "date", "software", "chat", "home", "bff", 
+    "think", "go", "harass", "poem"],
     transitions=[
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
+            "dest": "start",
+            "conditions": "is_going_to_start",
         },
         {
             "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
+            "source": "start",
+            "dest": "software",
+            "conditions": "is_going_to_software",
         },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+        {
+            "trigger": "advance",
+            "source": "start",
+            "dest": "language",
+            "conditions": "is_going_to_language",
+        },
+        {
+            "trigger": "advance",
+            "source": "language",
+            "dest": "chinese",
+            "conditions": "is_going_to_chinese",
+        },
+        {
+            "trigger": "advance",
+            "source": "language",
+            "dest": "german",
+            "conditions": "is_going_to_german",
+        },
+        {
+            "trigger": "advance",
+            "source": "language",
+            "dest": "french",
+            "conditions": "is_going_to_french",
+        },
+        {
+            "trigger": "advance",
+            "source": "chinese",
+            "dest": "prison",
+            "conditions": "is_going_to_prison",
+        },
+        {
+            "trigger": "advance",
+            "source": "chinese",
+            "dest": "nobody",
+            "conditions": "is_going_to_nobody",
+        },
+        {
+            "trigger": "advance",
+            "source": "chinese",
+            "dest": "friend",
+            "conditions": "is_going_to_friend",
+        },
+        {
+            "trigger": "advance",
+            "source": "friend",
+            "dest": "chat",
+            "conditions": "is_going_to_chat",
+        },
+        {
+            "trigger": "advance",
+            "source": "friend",
+            "dest": "home",
+            "conditions": "is_going_to_home",
+        },
+        {
+            "trigger": "advance",
+            "source": "chat",
+            "dest": "date",
+            "conditions": "is_going_to_date",
+        },
+        {
+            "trigger": "advance",
+            "source": "chat",
+            "dest": "bff",
+            "conditions": "is_going_to_bff",
+        },
+        {
+            "trigger": "advance",
+            "source": "date",
+            "dest": "think",
+            "conditions": "is_going_to_think",
+        },
+        {
+            "trigger": "advance",
+            "source": "date",
+            "dest": "go",
+            "conditions": "is_going_to_go",
+        },
+        {
+            "trigger": "advance",
+            "source": "german",
+            "dest": "harass",
+            "conditions": "is_going_to_harass",
+        },
+        {
+            "trigger": "advance",
+            "source": "german",
+            "dest": "poem",
+            "conditions": "is_going_to_poem",
+        },
+        {
+            "trigger": "advance",
+            "source": "poem",
+            "dest": "nobody",
+            "conditions": "is_going_to_nobody",
+        },
+        {
+            "trigger": "advance",
+            "source": "poem",
+            "dest": "bff",
+            "conditions": "is_going_to_bff",
+        },
+        {"trigger": "go_back", "source": ["prison", "nobody", "date", "software", "home", "bff", "go", "think", "harass", "poem", "french"],
+        "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
@@ -94,6 +201,7 @@ def webhook_handler():
 
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
+        mstate=machine.state
         if not isinstance(event, MessageEvent):
             continue
         if not isinstance(event.message, TextMessage):
@@ -104,7 +212,8 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
+            send_text_message(event.reply_token, mstate)
+            #send_text_message(event.reply_token, mstate)
 
     return "OK"
 
@@ -117,4 +226,4 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="127.0.0.1", port=port, debug=True)
--- fsm.py
+++ fsm.py
@@ -1,36 +1,405 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import send_text_message, send_button_message
+from linebot.models import MessageTemplateAction
+from random import randrange
+from translate import Translator
+
+#global var
+name=''
+posibilities=100
+lang=''
+need_translate=''
 
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_start(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return bool(text.strip())
 
-    def is_going_to_state2(self, event):
+    def is_going_to_software(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "初音"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def is_going_to_chinese(self, event):
+        text = event.message.text
+        return text.lower() == "中文"
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+    def is_going_to_language(self, event):
+        global name
+        text = event.message.text
+        name = text
+        return text.lower() != "初音" and bool(text.strip())
+
+    def is_going_to_german(self, event):
+        text = event.message.text
+        return text.lower() == "德文"
+
+    def is_going_to_french(self, event):
+        text = event.message.text
+        return text.lower() == "法文"
+
+    def is_going_to_prison(self, event):
+        text=event.message.text
+        return text.lower() == "steal"
+
+    def is_going_to_nobody(self, event):
+        text=event.message.text
+        return text.lower() == "return"
+
+    def is_going_to_friend(self, event):
+        text=event.message.text
+        return text.lower() == "gold_or_silver"
+
+    def is_going_to_chat(self, event):
+        text=event.message.text
+        return text.lower() == "chat"
+
+    def is_going_to_home(self, event):
+        text=event.message.text
+        return text.lower() == "home"
+
+    def is_going_to_date(self, event):
+        text=event.message.text
+        return text.lower() == "date"
+
+    def is_going_to_bff(self, event):
+        text=event.message.text
+        return text.lower() == "bff"
+
+
+    def is_going_to_go(self, event):
+        text=event.message.text
+        return text.lower() == "go"
+
+    def is_going_to_think(self, event):
+        text=event.message.text
+        return text.lower() == "think"
+
+    def is_going_to_harass(self, event):
+        text=event.message.text
+        return text.lower() == "showgun"
+
+    def is_going_to_poem(self, event):
+        text=event.message.text
+        return text.lower() != "showgun" and bool(text.strip())
+
+    def on_enter_software(self, event):
+        title = '醒醒吧。'
+        text = '初音只是個軟體( ｀д′)'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭開始',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
         self.go_back()
 
     def on_exit_state1(self):
         print("Leaving state1")
 
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
+    def on_enter_start(self, event):
+        title = '邂逅開始'
+        text = '請輸入(或選擇)一個名字成為你的邂逅對象'
+        btn = [
+            MessageTemplateAction(
+                label = '莊坤達',
+                text ='莊坤達'
+            ),
+            MessageTemplateAction(
+                label = '初音',
+                text = '初音'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+
+    def on_enter_language(self, event):
+        global name
+        title = '關於' + name
+        text = '請問他說什麼語言？'
+        btn = [
+            MessageTemplateAction(
+                label = '德文',
+                text ='德文'
+            ),
+            MessageTemplateAction(
+                label = '法文',
+                text = '法文'
+            ),
+            MessageTemplateAction(
+                label = '中文',
+                text = '中文'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    def on_enter_chinese(self, event):
+        global name
+        title = '有天走在路上你看到'+name+'在面前把錢包掉在地上'
+        text = '你會...'
+        btn = [
+            MessageTemplateAction(
+                label = '喊住他，歸還',
+                text ='return'
+            ),
+            MessageTemplateAction(
+                label = '問他是掉金錢包還是銀錢包',
+                text = 'gold_or_silver'
+            ),
+            MessageTemplateAction(
+                label = '自己偷偷暗崁起來',
+                text = 'steal'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    def on_enter_prison(self, event):
+        title = '利慾薰心。'
+        text = '是什麼蒙蔽了我的雙眼？'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭開始',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
         self.go_back()
 
+    def on_enter_nobody(self, event):
+        title = '邂逅結束。'
+        text = '你是個好人，但沒有緣份。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭開始',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+
+    def on_enter_friend(self, event):
+        global name
+        title = '認識'
+        text = name + '被你的話逗樂了，你決定...'
+        btn = [
+            MessageTemplateAction(
+                label = '多聊幾句',
+                text = 'chat'
+            ),
+            MessageTemplateAction(
+                label = '回家吧，好累喔。',
+                text = 'home'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    def on_enter_home(self, event):
+        global name
+        title = '休息是為了下一次的邂逅。'
+        text = name + '與你就像兩條水平線，從此沒有交集。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭來過',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+    
+    def on_enter_chat(self, event):
+        global name
+        title = '邀約'
+        text = name + '跟你的興趣、個性都很合，所以你決定..'
+        btn = [
+            MessageTemplateAction(
+                label = '約會',
+                text = 'date'
+            ),
+            MessageTemplateAction(
+                label = '繼續當朋友',
+                text = 'bff'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+
+    def on_enter_bff(self, event):
+        global name
+        title = '相見恨晚'
+        text = name + '與你成為一生的摯友。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭來過',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+
+    def on_enter_date(self, event):
+        global name
+        title = '朋友以上？'
+        text = '幾個月過去了，你跟' + name + '的關係越來越好，已經約會過了幾次，你確信你們互有好感，你決定...'
+        btn = [
+            MessageTemplateAction(
+                label = '衝一波',
+                text = 'go'
+            ),
+            MessageTemplateAction(
+                label = '再想想好了',
+                text = 'think'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    def on_enter_think(self, event):
+        global name
+        title = '躊躇不定。'
+        text = name + '覺得你對他沒興趣，跟別人在一起了。請祝福他。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭開始',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+
+    def on_enter_go(self, event):
+        global name
+        global posibilities
+        success=randrange(posibilities)
+        if success==0:
+            #reset posibilities
+            title = '命中注定。'
+            text = '你們開始交往了，當初他落掉的錢包難道就是命中注定？'
+            btn = [
+                MessageTemplateAction(
+                    label = '從頭開始',
+                    text = 'back'
+                ),
+                MessageTemplateAction(
+                    label = str(posibilities),
+                    text = 'back'
+                ),
+            ]
+            url = 'https://imgur.com/7cTQOVP.jpg'
+            send_button_message(event.reply_token, title, text, btn, url)
+        else: 
+            title = '再接再厲。'
+            text = '你是個好人，' + name + '說。'
+            btn = [
+                MessageTemplateAction(
+                    label = '從頭開始',
+                    text = 'back'
+                ),
+                MessageTemplateAction(
+                    label = str(posibilities),
+                    text = 'back'
+                ),
+            ]
+            url = 'https://imgur.com/7cTQOVP.jpg'
+            send_button_message(event.reply_token, title, text, btn, url)
+        
+        self.go_back()
+        
+    def on_enter_german(self, event):
+        global lang, need_translate
+        lang='de'
+        title = '我講中文他講德文，聽得懂嗎？'
+        text = '請輸入(或選擇)中文語句'
+        btn = [
+            MessageTemplateAction(
+                label = '要修(釣魚)竿嗎',
+                text = 'showgun'
+            ),
+            MessageTemplateAction(
+                label = '白日依山盡',
+                text = '白日依山盡'
+            ),
+        ]
+        need_translate=text
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    
+    def on_enter_poem(self, event):
+        global lang, need_translate
+        translator=Translator(provider='mymemory', to_lang=lang, from_lang="zh-TW")
+        translation=translator.translate(need_translate)
+        title = '要回答嗎？'
+        
+        #in case the message is to long
+        if len(translation)>60:
+            translation=translation[0:60]
+        
+        text = translation
+        btn = [
+            MessageTemplateAction(
+                label = '要',
+                text = 'bff'
+            ),
+            MessageTemplateAction(
+                label = '不要',
+                text = 'return'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
+    
+    def on_enter_harass(self, event):
+        title = '原來他聽得懂中文。'
+        text = '你因性騷擾到警局備案。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭來過',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+
+    def on_enter_french(self, event):
+        title = '作者覺得法文難得要死0.0。'
+        text = '乖，不要選這個選項。'
+        btn = [
+            MessageTemplateAction(
+                label = '從頭來過',
+                text = 'back'
+            ),
+        ]
+        url = 'https://imgur.com/7cTQOVP.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+        self.go_back()
+
+
+    def on_enter_user(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "輸入'start'重新開始")
+
+    
     def on_exit_state2(self):
         print("Leaving state2")
+
+    #def is_going_to_state3(self, event):
+
+
